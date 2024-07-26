628398ce8b28d5ac8eb76e94a61bac588e7d1d6e
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2024-07-26 16:44:07 -0600

    cart add && delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,90 @@
-import { useEffect, useState } from "react"
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-import { db } from "./data/db"
-
+import { useEffect, useState } from 'react';
+import Guitar from './components/Guitar';
+import Header from './components/Header';
+import { db } from './data/db';
 
 function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
-    const [data, setData] = useState(db)
-    const [cart, setCart] = useState([])
+  const MAX_ITEMS = 5;
 
-    return (
-        <>
-            <Header />
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      // existe en el carrito
+      const updateCart = [...cart];
+      updateCart[itemExists].quantity++;
+      setCart(updateCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
-            <main className="container-xl mt-5">
-                <h2 className="text-center">Nuestra Colección</h2>
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
 
-                <div className="row mt-5">
-                    {data.map( (guitar) => ( 
-                        <Guitar
-                        key={guitar.id} 
-                        guitar={guitar}
-                        setCart={setCart}
-                        />
-                    ))}
-                </div>
-            </main>
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
 
+  function decrementQuantity(id) {
+    const deletCart = cart.map((item) => {
+      if (item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(deletCart);
+  }
 
-            <footer className="bg-dark mt-5 py-5">
-                <div className="container-xl">
-                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-                </div>
-            </footer>
-        </>
-    )
+  return (
+    <>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
+      />
+
+      <main className='container-xl mt-5'>
+        <h2 className='text-center'>Nuestra Colección</h2>
+
+        <div className='row mt-5'>
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className='bg-dark mt-5 py-5'>
+        <div className='container-xl'>
+          <p className='text-white text-center fs-4 mt-4 m-md-0'>
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
